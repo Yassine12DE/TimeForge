289e3c1bbae4cccbb2503e4ac3827926054979d4
Yassine12DE <yassine.bouabdallah@esprit.tn>
2025-02-10 22:27:58 +0100

    Intitial commit with new project by Si Yassine Bouabdallah

---
+++ Backend/TimeForgeBackend/src/main/java/tn/esprit/tic/TimeForgeBackend/TimeForgeBackendApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.tic.TimeForgeBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeForgeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeForgeBackendApplication.class, args);
+	}
+
+}
---
+++ Backend/TimeForgeBackend/src/test/java/tn/esprit/tic/TimeForgeBackend/TimeForgeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.tic.TimeForgeBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeForgeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
